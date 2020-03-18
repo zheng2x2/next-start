2bee55cb617a243ee198e8429979495016ace2a8
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-18 23:29:50 +0900

    Link component w/prefetch

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,9 @@ const Header = () => {
         <div>
             <Link href="/"><a style={linkStyle}>Home</a></Link>
             <Link href="/about"><a style={linkStyle}>About</a></Link>
-            <Link href="/ssr-test"><a style={linkStyle}>SSR 테스트</a></Link>
+            <Link prefetch href="/ssr-test"><a style={linkStyle}>SSR 테스트</a></Link>
+            {/* 링크 컴포넌트를 렌더링할때 <Link prefetch href="..."> 형식으로 
+            prefetch 값을 전달해주면 데이터를 먼저 불러온다음에 라우팅을 시작합니다. */}
         </div>
     )
 }
